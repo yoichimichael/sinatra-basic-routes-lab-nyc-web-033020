a5259d6c73e8682d09308a6e1c7b6e101ede3bf6
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-04-19 17:49:37 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Yoichi"
+  end
+
+  get '/hometown' do
+    "My hometown is Brookline"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Backstabbers"
+  end
+
 end
